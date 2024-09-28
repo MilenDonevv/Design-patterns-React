deb8ea265457d9fa4ae9180f46c4dbc1ad150503
MilenDonevv <milendonev95@gmail.com>
2024-09-28 18:30:44 +0300

    add different desing-styled components

---
+++ 1-Layout-Components/src/designs/modals.jsx
@@ -0,0 +1,24 @@
+/*
+
+
+<div styles={...}>
+    <h1>Component Code</h1> 
+</div>
+
+
+-----------
+
+<div styles={...}>
+    {children}
+</div>
+
+<>
+    <h1>Component Code</h1>
+</>
+
+Separation between the component itself and where that component is being displayed on the page.
+
+--> Components should't know where they're being displayed !! 
+
+
+*/
---
+++ 1-Layout-Components/src/designs/split-screens.jsx
@@ -0,0 +1,30 @@
+/*
+
+
+<div styles={...}>
+    <h1>Component Code</h1> 
+</div>
+
+
+-----------
+
+<div styles={...}>
+    {children}
+</div>
+
+<>
+    <h1>Component Code</h1>
+</>
+
+Separation between the component itself and where that component is being displayed on the page.
+
+--> Components should't know where they're being displayed !! 
+
+----------
+
+npm install styled-components 
+
+--> Very easy way to add styles to react components
+
+
+*/
