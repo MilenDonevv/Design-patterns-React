883ea80d22bdc63de38866e5580aef105a2139b7
MilenDonevv <milendonev95@gmail.com>
2024-09-29 22:28:08 +0300

    add styled-components package

--- 1-Layout-Components/src/designs/split-screens.jsx
+++ 1-Layout-Components/src/designs/split-screens.jsx
@@ -28,3 +28,45 @@ npm install styled-components
 
 
 */
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Pane = styled.div`
+    flex: 1;
+`;
+
+// export const SplitScreen = ({
+//     left: Left,                      // property left has a value the Component Left
+//     right: Right,
+// }) => {
+//     return (
+//         <div>
+//             <div>
+//                 <Left />
+//             </div>
+//             <div>
+//                 <Right />
+//             </div>
+//         </div>
+//     )
+// }
+
+export const SplitScreen = ({
+    left: Left,                      // property left has a value the Component Left
+    right: Right,
+}) => {
+    return (
+        <Container>
+            <Pane>
+                <Left />
+            </Pane>
+            <Pane>
+                <Right />
+            </Pane>
+        </Container>
+    )
+}
